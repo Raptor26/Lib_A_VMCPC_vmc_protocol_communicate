02fc71646bec7eef3ca29314dc7c9daab2d5e550
Isaev Mickle <mrraptor26@gmail.com>
2018-10-08 12:53:10 +0300

    feature (Lib_A_VMCPC_vmc_protocol_communicate) add new functions and structs

--- Lib_A_VMCPC_vmc_protocol_communicate.c
+++ Lib_A_VMCPC_vmc_protocol_communicate.c
@@ -1,7 +1,7 @@
-/** 
+/**
  * @file   	%<%NAME%>%.%<%EXTENSION%>%
  * @author 	%<%USER%>%
- * @version	
+ * @version
  * @date 	%<%DATE%>%, %<%TIME%>%
  * @brief
  */
@@ -17,6 +17,9 @@
 
 
 /*#### |Begin| --> Секция - "Локальные переменные" ###########################*/
+static uint16_t
+VMCPC_LEGACY_CalcCRC16For3Motors(
+	vmcpc_legacy_package_for_3_motors_s *p_s);
 /*#### |End  | <-- Секция - "Локальные переменные" ###########################*/
 
 
@@ -25,10 +28,65 @@
 
 
 /*#### |Begin| --> Секция - "Описание глобальных функций" ####################*/
+void
+VMCPC_LEGACY_SetCotrolFor3Motors(
+	vmcpc_legacy_package_for_3_motors_s *p_s,
+	__VMCPC_FPT__ roll,
+	__VMCPC_FPT__ pitch,
+	__VMCPC_FPT__ yaw)
+{
+	/* Условие начала пакета данных */
+	p_s->startFrame = VMVPC_START_FRAME_1_BYTE;
+
+	/* Заполнение массива управления. Значения из этого массива будут
+	 * применены к 3-м электродвигателям */
+	p_s->actualControl_a[VMCPC_VECT_X_ROLL] 	= roll;
+	p_s->actualControl_a[VMCPC_VECT_Y_PITCH] 	= pitch;
+	p_s->actualControl_a[VMCPC_VECT_Z_YAW] 		= yaw;
+
+	/* Расчет контрольной суммы пакета данных и копирование в конец пакета
+	 * данных */
+	p_s->poly0x1021_crc16 =
+		VMCPC_LEGACY_CalcCRC16For3Motors(p_s);
+}
+
+__VMCPC_FPT__
+VMCPC_LEGACY_ParceControlPackageFor3Motors(
+	vmcpc_legacy_package_for_3_motors_s *p_s,
+	vmcpc_who_i_am_e whoIAm)
+{
+	/* Если начало пакета и контрольная сумма верны */
+	if ((p_s->startFrame == VMVPC_START_FRAME_1_BYTE)
+			&& (p_s->poly0x1021_crc16 == VMCPC_LEGACY_CalcCRC16For3Motors(p_s)))
+	{
+		if (whoIAm == VMCPC_I_AM_ROLL)
+		{
+			return ((__VMCPC_FPT__) p_s->actualControl_a[VMCPC_VECT_X_ROLL]);
+		}
+		else if (whoIAm == VMCPC_I_AM_PITCH)
+		{
+			return ((__VMCPC_FPT__) p_s->actualControl_a[VMCPC_VECT_Y_PITCH]);
+		}
+		else
+		{
+			return ((__VMCPC_FPT__) p_s->actualControl_a[VMCPC_VECT_Z_YAW]);
+		}
+	}
+	return ((__VMCPC_FPT__)0.0);
+}
+
 /*#### |End  | <-- Секция - "Описание глобальных функций" ####################*/
 
 
 /*#### |Begin| --> Секция - "Описание локальных функций" #####################*/
+uint16_t
+VMCPC_LEGACY_CalcCRC16For3Motors(
+	vmcpc_legacy_package_for_3_motors_s *p_s)
+{
+	return (CRC_XOR_CCITT_Poly0x1021_Crc16(
+				(uint8_t*)p_s,
+				sizeof(vmcpc_legacy_package_for_3_motors_s) - VMCPC_LEGACY_PACKAGE_FOR_3_MOTORS_S_BYTES_NUMB));
+}
 /*#### |End  | <-- Секция - "Описание локальных функций" #####################*/
 
 
--- Lib_A_VMCPC_vmc_protocol_communicate.h
+++ Lib_A_VMCPC_vmc_protocol_communicate.h
@@ -1,7 +1,7 @@
-/** 
+/**
  * @file   	%<%NAME%>%.%<%EXTENSION%>%
  * @author 	%<%USER%>%
- * @version	
+ * @version
  * @date 	%<%DATE%>%, %<%TIME%>%
  * @brief
  */
@@ -14,6 +14,7 @@
 /*#### |Begin| --> Секция - "Include" ########################################*/
 /*==== |Begin| --> Секция - "C libraries" ====================================*/
 #include <math.h>
+#include <stdint.h>
 /*==== |End  | <-- Секция - "C libraries" ====================================*/
 
 /*==== |Begin| --> Секция - "MK peripheral libraries" ========================*/
@@ -26,10 +27,58 @@
 
 
 /*#### |Begin| --> Секция - "Определение констант" ###########################*/
+#if !defined (__VMCPC_FPT__)	///< __VMCPC_FPT__ - FLOAT_POINT_TYPE
+#error "Please, set value for macros __VMCPC_FPT__ float or double"
+#endif
+
+#define VMVPC_START_FRAME_1_BYTE				((uint8_t)0xAA)
 /*#### |End  | <-- Секция - "Определение констант" ###########################*/
 
 
 /*#### |Begin| --> Секция - "Определение типов" ##############################*/
+typedef enum
+{
+	VMCPC_VECT_X_ROLL = 0,
+	VMCPC_VECT_Y_PITCH,
+	VMCPC_VECT_Z_YAW,
+
+	VMCPC_SIZE_VECT_ARR,
+} vmcpc_legacy_vector_control_for_3_motors_e;
+
+typedef enum
+{
+	VMCPC_I_AM_ROLL 	= VMCPC_VECT_X_ROLL,
+	VMCPC_I_AM_PITCH 	= VMCPC_VECT_Y_PITCH,
+	VMCPC_I_AM_YAW 		= VMCPC_VECT_Z_YAW,
+} vmcpc_who_i_am_e;
+
+/**
+ * @brief	Структура пакета данных для управления 3-мя электродвигателями.
+ * 			Три электродвигателя принимают данный пакет данных одновременно, в
+ * 			зависимости от того, какой осью управляет электродвигатель,
+ * 			в качестве управления берется соответствующая ячейка из массива
+ * 			"actualControl_a":
+ * 				- если двигатель управляет осью крена, то берется 0-я ячейка массива
+ *					@arg VMCPC_VECT_X_ROLL
+ * 				- если двигатель управляет осью тангажа, то берется 1-я ячейка массива VMCPC_VECT_Y_PITCH
+ * 					@arg VMCPC_VECT_Y_PITCH
+ * 				- если двигатель управляет осью азимута, то берется 2-я ячейка массива VMCPC_VECT_Z_YAW
+ * 					@arg VMCPC_VECT_Z_YAW
+ */
+typedef struct
+{
+	/* Условие начала пакета данных */
+	uint8_t startFrame;
+
+	/* Массив, в котором содержится значение управления для каждой из 3-х осей */
+	__VMCPC_FPT__ actualControl_a[VMCPC_SIZE_VECT_ARR];
+
+	/* Контрольная сумма пакета данных */
+	uint16_t poly0x1021_crc16;
+}
+__attribute__((__packed__))
+vmcpc_legacy_package_for_3_motors_s;
+#define VMCPC_LEGACY_PACKAGE_FOR_3_MOTORS_S_BYTES_NUMB	2
 /*#### |End  | <-- Секция - "Определение типов" ##############################*/
 
 
@@ -38,6 +87,17 @@
 
 
 /*#### |Begin| --> Секция - "Прототипы глобальных функций" ###################*/
+extern void
+VMCPC_LEGACY_SetCotrolFor3Motors(
+	vmcpc_legacy_package_for_3_motors_s *p_s,
+	__VMCPC_FPT__ roll,
+	__VMCPC_FPT__ pitch,
+	__VMCPC_FPT__ yaw);
+
+extern __VMCPC_FPT__
+VMCPC_LEGACY_ParceControlPackageFor3Motors(
+	vmcpc_legacy_package_for_3_motors_s *p_s,
+	vmcpc_who_i_am_e whoIAm);
 /*#### |End  | <-- Секция - "Прототипы глобальных функций" ###################*/
 
 
